23a51cb35a2774690e4c502af5db9cbd420cef50
cialloo <aluckywenda@gmail.com>
2023-10-12 07:26:35 +0800

    add navigate size

--- lib/app_navigate_field.dart
+++ lib/app_navigate_field.dart
@@ -7,12 +7,19 @@ class AppNavigateFieldNotifier with ChangeNotifier {}
 class AppNavigateField extends StatelessWidget {
   const AppNavigateField({super.key});
 
+  final double _widthFactor = 0.2;
+
   @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
       listenable: kAppNavigateNotifier,
       builder: (context, child) {
-        return const Text('todo field');
+        return SizedBox(
+          width: MediaQuery.of(context).size.width * _widthFactor,
+          child: const Column(
+            children: [],
+          ),
+        );
       },
     );
   }
