c489397bcb934e4a723e58af5dddc5b685df176a
cialloo <aluckywenda@gmail.com>
2023-10-11 17:34:56 +0800

    add login page as init page

---
+++ lib/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(
+        builder: (context) {
+          return Center(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: Column(children: [
+                const TextField(),
+                const TextField(),
+                TextButton(onPressed: () {}, child: const Text('login'))
+              ]),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
