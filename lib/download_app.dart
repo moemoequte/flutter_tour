50b44fd60dbf5c01ccb81ef1df6ae346a733ff27
cialloo <aluckywenda@gmail.com>
2023-10-12 22:19:03 +0800

    fix func not run

--- lib/download_app.dart
+++ lib/download_app.dart
@@ -16,36 +16,49 @@ class DownloadApp extends StatelessWidget {
         SizedBox(
             height: 20,
             width: MediaQuery.of(context).size.width * 0.6,
-            child: ListenableBuilder(builder: (context, child) {
-              return LinearProgressIndicator(value: _value,);
-            }, listenable: _progressNotifier,)),
+            child: ListenableBuilder(
+              builder: (context, child) {
+                return LinearProgressIndicator(
+                  value: _value,
+                );
+              },
+              listenable: _progressNotifier,
+            )),
         const SizedBox(
           height: 10,
         ),
-        ElevatedButton(onPressed: () async {
-          bool isDone = false;
-          int contentLength = 0;
-          int tempLength = 0;
-          List<int> list = List.empty(growable: true);
-          () async {
-            final response = await http.Client().send(http.Request('GET', Uri.parse('https://upload.wikimedia.org/wikipedia/commons/f/ff/Pizigani_1367_Chart_10MB.jpg')));
-            contentLength = response.contentLength ?? 0;
-            response.stream.listen((value) { 
-              tempLength += value.length;
-              _value = tempLength/contentLength;
-              list.addAll(value);
-            }).onDone(() {
-              File file = File('./a.jpg');
-              file.createSync();
-              file.writeAsBytesSync(list);
-              isDone = true;
-            });
-          };
-          while(!isDone) {
-            await Future.delayed(Duration(milliseconds: 200));
-            _progressNotifier.changeValue();
-          }
-        }, child: const Text('Download')),
+        ElevatedButton(
+            onPressed: () async {
+              bool isDone = false;
+              int contentLength = 0;
+              int tempLength = 0;
+              List<int> list = List.empty(growable: true);
+              downloadFunc() async {
+                debugPrint('start download');
+                final response = await http.Client().send(http.Request(
+                    'GET',
+                    Uri.parse(
+                        'https://upload.wikimedia.org/wikipedia/commons/f/ff/Pizigani_1367_Chart_10MB.jpg')));
+                contentLength = response.contentLength ?? 0;
+                response.stream.listen((value) {
+                  tempLength += value.length;
+                  _value = tempLength / contentLength;
+                  list.addAll(value);
+                }).onDone(() {
+                  File file = File('./a.jpg');
+                  file.createSync();
+                  file.writeAsBytesSync(list);
+                  isDone = true;
+                });
+              }
+
+              downloadFunc();
+              while (!isDone) {
+                await Future.delayed(const Duration(milliseconds: 200));
+                _progressNotifier.changeValue();
+              }
+            },
+            child: const Text('Download')),
         const Spacer()
       ],
     );
